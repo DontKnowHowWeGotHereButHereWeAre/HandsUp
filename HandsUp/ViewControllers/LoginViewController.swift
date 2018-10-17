cfabc90c12c3085a91624dec777d8cd6ba6d7dc0
Nikhil Menon <15204718+Menonn0228@users.noreply.github.com>
2018-10-17 18:47:36 -0500

    Created a signup button. Button needs to be connected to the storyboard

--- HandsUp/ViewControllers/LoginViewController.swift
+++ HandsUp/ViewControllers/LoginViewController.swift
@@ -7,29 +7,46 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
     
     @IBOutlet weak var passwordField: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
     
     @IBAction func didTapNewUser(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: ButtonTapped to Sign Up
+    @IBAction func onSignUp(_sender: Any){
+        let newUser = PFUser();
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success{
+                print("User Signed Up Successfully!");
+                
+                //TODO: Segue into the HomeView
+                
+            }else{
+                print("error signing up: \(error!.localizedDescription)")
+                
+                if error?._code == 202{
+                    print("Username is taken")
+                }
+            }
+        }
     }
-    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
 
 }
